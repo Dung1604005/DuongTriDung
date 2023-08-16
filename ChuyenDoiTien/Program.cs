17446c6646896e2d417a64d910472bc3fa1e2b8d
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-16 22:24:27 +0700

    ChuyenDoiTien

---
+++ ChuyenDoiTien/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        const int rate = 23000;
+        float usd ;
+        usd = float.Parse(Console.ReadLine());
+        float vnd = usd*rate;
+        Console.WriteLine(usd + "USD = " + vnd + "VND");
+    }
+}
