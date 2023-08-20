cc81b4b990976df2d87b9fffc03e2437473c7455
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-20 22:04:57 +0700

    BMI-Cal

---
+++ BMI-Cal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+public class Program{
+    static void Main(string[] args)
+    {
+        float height;
+        float weight;
+        Console.Write("Please enter your height: ");
+        height = float.Parse(Console.ReadLine());
+        Console.Write("Please enter your weight: ");
+        weight = float.Parse(Console.ReadLine());
+        double bmi = weight / (height*height);
+        bmi = Math.Round(bmi, 2);
+        if (bmi < 18.5f){
+            Console.Write("Underweight");
+        }
+        else if (bmi < 25f){
+            Console.Write("Normal");
+        }
+        else if (bmi < 30f){
+            Console.Write("Overweight");
+        }
+        else {
+            Console.Write("Obese");
+        }
+    }
+}
