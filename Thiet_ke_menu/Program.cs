a24d2db488f43684aad114c1587fd26f30c2cd7c
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-22 23:59:27 +0700

    Thiet_ke_menu

---
+++ Thiet_ke_menu/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int choice;
+        do
+        {
+            Console.Write("\nMenu\n");
+            Console.Write("1.Draw the triangle\n");
+            Console.Write("2.Draw the square\n");
+            Console.Write("3.Draw the rectangle\n");
+            Console.Write("0.Exit\n");
+            Console.Write("Enter your choice: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice){
+                case 1:
+                    Console.Write("* * * * *\n");
+                    Console.Write("* * * *\n");
+                    Console.Write("* * *\n");
+                    Console.Write("* *\n");
+                    Console.Write("*");
+                    break;
+                case 2:
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    break;
+                case 3:
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    Console.Write("* * * * * *\n");
+                    break;
+            }
+        
+        } while(choice != 0);
+        
+        
+    }
+}
