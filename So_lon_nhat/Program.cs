98a1173d12d0fa2572c3893e820428846a9d40e5
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-23 21:16:00 +0700

    So_lon_nhat

---
+++ So_lon_nhat/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int n;
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        for(int i =0; i < n; i++){
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            if ( max < arr[i] ){
+                max = arr[i];
+            }
+        }
+        Console.Write(max);
+
+    }
+}
