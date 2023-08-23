0016c27ab08963fda8ad8a94ca77fe4772f493b1
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-23 19:20:50 +0700

    Kiem_tra_snt

---
+++ Kiem_tra_snt/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.Write("Nhap so n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        if ( n == 1 || n == 0){
+            Console.Write("Khong phai so nguyen to");
+        }
+        else if (n == 2){
+            Console.Write("Day la so nguyen to");
+        }
+        else{
+            string output = "Day la so nguyen to";
+            for(int i =2 ; i*i < n; i++){
+                if ( n % i == 0){
+                    output = "Khong phai la so nguyyen to";
+                }
+
+            }
+            Console.Write(output);
+        }
+    }
+}
