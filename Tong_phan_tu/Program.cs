2b8cbf060456f44b487ee86102f54716a4f98200
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-23 21:13:15 +0700

    Tong_phan_tu

---
+++ Tong_phan_tu/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int n;
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        for(int i =0; i < n; i++){
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        Console.Write(sum);
+
+    }
+}
