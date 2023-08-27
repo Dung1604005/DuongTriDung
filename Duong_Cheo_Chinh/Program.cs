06355bc30231889bc2c151ce3a4ace816114ea73
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-27 22:30:42 +0700

    Duong_Cheo_Chinh

---
+++ Duong_Cheo_Chinh/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Nhap so hang va cot: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Hay nhap phan tu trong bang");
+        int[,] ma_tran = new int[n, n];
+        for(int i = 0; i < n; i++ ){
+            for(int j = 0; j < n; j++){
+                ma_tran[i, j] = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++ ){
+            sum += ma_tran[i, i];
+                
+
+        }
+            
+        
+        Console.WriteLine(sum);
+    }
+}
