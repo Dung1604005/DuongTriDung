7ebe58f2dac48bdebc5c0c9d228f5025f63f0337
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-30 00:03:08 +0700

    Count

---
+++ Count/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        
+    }
+    static int count(string str, char a ){
+        int count = 0;
+        for(int i = 0; i < str.Length; i++){
+            if(str[i] == a){
+                count++;
+            }
+        }
+        return count;
+    }
+}
