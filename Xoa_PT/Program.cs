e77c050b9ff97beb793d23ecc23feebc088ee0b9
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-29 23:59:49 +0700

    Xoa_PT

---
+++ Xoa_PT/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int[] arr = {10, 4, 5, 7, 8};
+        Delete(arr, 10);
+        for(int i = 0; i < arr.Length-1; i++){
+            Console.WriteLine(arr[i]);
+        }
+    }
+    static void Delete(int[] arr, int x){
+        int index = 0;
+        for(int i = 0; i < arr.Length; i++){
+            if (arr[i] == x){
+                index = i;
+            }
+        }
+        for(int i = index; i < arr.Length-1; i++){
+            arr[i] = arr[i+1];
+        }
+    }
+}
