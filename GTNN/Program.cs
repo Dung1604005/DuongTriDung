558599815740d303364ee16a61fee4be0f265d0d
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-08-29 23:52:52 +0700

    GTNN

---
+++ GTNN/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        int[] arr = {1,4, 10, 22, 223};
+        Console.WriteLine(Min(arr));
+    }
+    public static int Min(int[] arr){
+        int min = arr[0];
+        for(int i = 0; i < arr.Length; i++ ){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
