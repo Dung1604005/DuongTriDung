4be90793533f17a4eca147d3074f979fc0208a86
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-09-13 00:42:04 +0700

    IColorable

---
+++ IColorable/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shape;
+
+public class Program{
+    static void Main(string[] args)
+    {
+        Shapes[] arr = new Shapes[4];
+        arr[0] = new Circle(3);
+        
+        
+        arr[1] = new Rectangle(3, 4);
+        arr[2] = new Square(3);
+        arr[3] = new Circle(1);
+        
+        
+        for(int i = 0; i < 4; i++){
+           if (arr[i] is Square){
+            Square square = new Square();
+            square.HowToColor();
+               
+           }
+           
+          
+        }
+    }
+}
