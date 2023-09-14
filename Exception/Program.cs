ec943444447a24a28511fb7478200e2f25831a10
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-09-14 22:18:49 +0700

    Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        try{
+            char[] character = new char[5];
+            character[0] = 'a';
+            character[1] = 'b';
+            character[2] = 'c';
+            character[3] = 'd';
+            character[4] = 'e';
+            int val = character[5];
+
+
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Wrong");
+
+        }
+
+    }
+}
