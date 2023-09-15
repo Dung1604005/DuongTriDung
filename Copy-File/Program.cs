933d6b67aef19d2ee74489198a2b8efa22e867e8
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-09-16 00:43:33 +0700

    Copy-File

---
+++ Copy-File/Program.cs
@@ -0,0 +1,37 @@
+﻿public static class Main
+{
+    private static void CopyFileUsingFileInfo(FileInfo fileinf, FineInfo des)
+    {
+        fileinf.CopyTo(des.FullName, true);
+
+    }
+    private static void CopyFileUsingStream(FileInfo fileinf, FineInfo des)
+    {
+        StreamReader read = null;
+        StreamWriter write = null;
+        try
+        {
+            reader = new StreamReader(source.FullName);
+            writer = new StreamWriter(des.FullName);
+            Char[] buffer = new Char[1024];
+            int length;
+            while ((length = reader.Read(buffer)) > 0)
+            {
+                writer.Write(buffer, 0, length);
+            }
+
+        }
+        finally
+        {
+            read.Close();
+            read.Dispose();
+            write.Close();
+            write.Dispose();
+        }
+    }
+    static void Main(string[] args)
+    {
+        
+    }
+
+}
