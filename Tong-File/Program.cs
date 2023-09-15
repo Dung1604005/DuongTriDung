85550e0e00750d203b9de137b0c341e25a260bff
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-09-16 00:31:49 +0700

    Tong-File

---
+++ Tong-File/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+    static void Main(string[] args)
+    {
+        string filepath = "Text.txt";
+        StreamReader read = new StreamReader(filepath);
+        int sum = 0;
+        string line;
+        while((line = read.ReadLine()) != null){
+            int p = Convert.ToInt32(line);
+            sum += p;
+
+        }
+        read.Close();
+        Console.WriteLine(sum);
+
+    }
+}
