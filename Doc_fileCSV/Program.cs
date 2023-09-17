9ec52e651064bb778ecf14cc3d04756da826e4df
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2023-09-17 21:18:36 +0700

    Doc_FileCSV

---
+++ Doc_fileCSV/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Prpgram{
+    static void Main(string[] args)
+    {
+        string filepath = "test.csv";
+        StreamReader read = new StreamReader(filepath);
+        StreamWriter write = new StreamWriter("output.txt");
+        string line;
+        
+        while((line = read.ReadLine()) != null){
+            string[] read_doc = line.Split(',');
+            
+            
+            for(int i = 1; i < read_doc[5].Length -1; i++){
+                write.Write(read_doc[5][i]);
+
+            }
+            write.WriteLine("");
+        
+
+
+        }
+        read.Close();
+        write.Close();
+    }
+}
